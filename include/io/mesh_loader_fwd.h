cb1e461fa80cddacbad816f5873a1a80aaf715a6
Anubhav-Mondal <anubhavmondal344@gmail.com>
2026-06-15 23:04:36 +0530

    feat: gltf format support

--- include/io/mesh_loader_fwd.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "geometry/hittable.h"
-#include "material/material.h"
-#include <string>
-#include <memory>
-
-shared_ptr<hittable> load_obj(const std::string& path, shared_ptr<material> mat);
--- src/stb_impl.cpp
+++ src/stb_impl.cpp
@@ -1,6 +1,2 @@
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
 #define TINYOBJLOADER_IMPLEMENTATION
-#include "stb_image.h"
-#include "stb_image_write.h"
 #include "io/mesh_loader.h"
